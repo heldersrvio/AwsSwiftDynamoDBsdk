8d529ccd005d6dd4af9cce7d3e3c09c9f458ed14
Helder Sérvio <heldersrvio@gmail.com>
2021-12-06 17:53:34 -0300

    FIX: AWSSIGN VERSION 0.4.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "AwsDynamoDB",
     platforms: [.macOS(.v10_12)],
     products: [.library(name: "AwsDynamoDB", targets: ["AwsDynamoDB"])],
-    dependencies: [.package(name: "AwsSign", url: "https://github.com/heldersrvio/AwsSwiftSign.git", from: "0.4.0")],
+    dependencies: [.package(name: "AwsSign", url: "https://github.com/heldersrvio/AwsSwiftSign.git", from: "0.4.1")],
     targets: [.target(name: "AwsDynamoDB", dependencies: ["AwsSign"]),
               .testTarget(name: "AwsDynamoDBTests", dependencies: ["AwsDynamoDB"])],
     swiftLanguageVersions: [.v5]
